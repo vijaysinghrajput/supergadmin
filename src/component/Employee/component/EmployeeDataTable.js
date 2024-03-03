d4024a5f27993979a9cc0754acee434e8b7c4425
Vijay Singh <iamvijaysinghrajput@gmail.com>
2024-03-03 13:40:14 +0530

    ok

--- src/component/Employee/component/EmployeeDataTable.js
+++ src/component/Employee/component/EmployeeDataTable.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
-import { FilterMatchMode, FilterOperator } from "primereact/api";
+import React, { useState, useEffect } from "react";
+import { FilterMatchMode } from "primereact/api";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { InputText } from "primereact/inputtext";
-import { MultiSelect } from "primereact/multiselect";
-import { Dropdown } from "primereact/dropdown";
-import { Tag } from "primereact/tag";
-import { Button } from "primereact/button";
+
 import { useNavigate } from "react-router";
 import { BiSearch } from "react-icons/bi";
 
@@ -28,13 +24,9 @@ export const EmployeeDataTable = () => {
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
     order_status: { value: null, matchMode: FilterMatchMode.EQUALS },
   });
-  const [isDataLoding, setisDataLoding] = useState(true);
   const [Sale, setSale] = useState(null);
 
-  const [selectedemployee, setSelectedemployee] = useState(null);
-
   const adminStoreId = cookies.get("adminStoreId");
-  const adminId = cookies.get("adminId");
 
   async function fetchData() {
     const data = await fetch(
@@ -58,7 +50,6 @@ export const EmployeeDataTable = () => {
 
   const {
     data: employee_list_load,
-    isError,
     isLoading: isLoadingAPI,
     isFetching,
   } = useQuery({
@@ -68,10 +59,8 @@ export const EmployeeDataTable = () => {
 
   useEffect(() => {
     setSale([]);
-    console.log("search product", employee_list_load, isLoadingAPI);
     if (employee_list_load) {
       setSale(employee_list_load.store_employee_list);
-      setisDataLoding(false);
     }
   }, [employee_list_load, isLoadingAPI]);
 
@@ -83,15 +72,6 @@ export const EmployeeDataTable = () => {
   //     { field: "order_status", header: "Status" },
   //     { field: "date", header: "Date" },
   //   ];
-
-  const statusBodyTemplate = (rowData) => {
-    return (
-      <Tag
-        value={rowData.order_status}
-        severity={getSeverity(rowData.order_status)}
-      ></Tag>
-    );
-  };
 
   const ActionBodyTemplate = (rowData) => {
     return (
@@ -111,53 +91,6 @@ export const EmployeeDataTable = () => {
     );
   };
 
-  const getSeverity = (value) => {
-    switch (value) {
-      case "Placed":
-        return "dark";
-      case "Confirmed":
-        return "success";
-      case "Preparing for dispatch":
-        return "warning";
-
-      case "On the way":
-        return "info";
-      case "Delivered":
-        return "primary";
-      case "Canceled":
-        return "danger";
-      case "Sold":
-        return "success";
-      default:
-        return null;
-    }
-  };
-
-  const [statuses] = useState([
-    "Placed",
-    "Confirmed",
-    "Preparing for dispatch",
-    "On the way",
-    "Delivered",
-    "Canceled",
-    "Sold",
-  ]);
-
-  const statusRowFilterTemplate = (options) => {
-    return (
-      <Dropdown
-        value={options.order_status}
-        options={statuses}
-        onChange={(e) => options.filterApplyCallback(e.value)}
-        itemTemplate={statusItemTemplate}
-        placeholder="Select Status"
-        className="p-column-filter"
-        showClear
-        style={{ minWidth: "5rem" }}
-      />
-    );
-  };
-
   const onGlobalFilterChange = (e) => {
     const value = e.target.value;
     let _filters = { ...filters };
@@ -168,50 +101,9 @@ export const EmployeeDataTable = () => {
     setGlobalFilterValue(value);
   };
 
-  const statusItemTemplate = (option) => {
-    return <Tag value={option} severity={getSeverity(option)} />;
-  };
-
   const renderHeader = () => {
     return (
       <div className="row  ">
-        <div className="col-sm-4">
-          <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">
-              <BiSearch />
-            </InputGroup.Text>
-            <Form.Control
-              placeholder="Search in Data"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-              value={globalFilterValue}
-              onChange={onGlobalFilterChange}
-            />
-          </InputGroup>
-        </div>
-        <div className="col-sm-4">
-          <Dropdown
-            value={statuses.order_status}
-            options={statuses}
-            onChange={(e) => statuses.filterApplyCallback(e.value)}
-            itemTemplate={statusItemTemplate}
-            placeholder="Select Year"
-            className="p-column-filter"
-            showClear
-            style={{ minWidth: "5rem" }}
-          />
-
-          <Dropdown
-            value={statuses.order_status}
-            options={statuses}
-            onChange={(e) => statuses.filterApplyCallback(e.value)}
-            itemTemplate={statusItemTemplate}
-            placeholder="Select Year"
-            className="p-column-filter"
-            showClear
-            style={{ minWidth: "5rem" }}
-          />
-        </div>
         <div className="col-sm-4">
           <InputGroup className="mb-3">
             <InputGroup.Text id="basic-addon1">
@@ -282,12 +174,6 @@ export const EmployeeDataTable = () => {
           header="Address"
           sortable
           style={{ width: "35%" }}
-        ></Column>
-        <Column
-          field="join_date"
-          header="Date"
-          sortable
-          style={{ width: "15%" }}
         ></Column>
 
         <Column
